896055569bf8b2f8f2fd04e46ae310af51a67779
Hamza Khalid <hamza.khalid@stixor.com>
2022-11-02 10:48:24 +0500

    Added otp stepper in signup page

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./signup.module.css";
 import upper_triangle from "../../public/assets/login_screen_assets/upper_triangle.png";
 import lower_triangle from "../../public/assets/login_screen_assets/lower_triangle.png";
@@ -7,6 +7,27 @@ import google_login_icon from "../../public/assets/login_screen_assets/google_lo
 import Link from "next/link";
 
 function SignUp() {
+  const [activeStep, setActiveStep] = useState(1);
+  const [otpSent, setOtpSent] = useState(false);
+
+  const handleNextStep = async () => {
+    if (activeStep === 3) {
+      if (!otpSent) {
+        handleSendOpt();
+      } else {
+        setActiveStep(1);
+      }
+    } else {
+      setActiveStep(activeStep + 1);
+    }
+  };
+
+  const handleSendOpt = async () => {
+    setOtpSent(true);
+  };
+
+  console.log(activeStep);
+
   return (
     <div className={classes.login_screen_section}>
       <div className={classes.carousel_section}>
@@ -28,26 +49,123 @@ function SignUp() {
             <span>Create</span> An Account
           </h2>
 
-          <div className={classes.input_field_contaier}>
-            <label>First Name</label>
-            <input
-              placeholder="Enter your First Name"
-              type="email"
-              className={classes.input_field}
-            />
-          </div>
-          <div className={classes.input_field_contaier}>
-            <label>Last Name</label>
-            <input
-              placeholder="Enter your Last Name"
-              type="email"
-              className={classes.input_field}
-            />
+          <div className={classes.stepper_container}>
+            <div
+              style={{ marginLeft: "0px" }}
+              className={
+                activeStep > 1
+                  ? classes.step_done
+                  : activeStep === 1 && classes.step_active
+              }
+            >
+              <p>1</p>
+            </div>
+            <div className={classes.dashed_line} />
+            <div
+              className={
+                activeStep > 2
+                  ? classes.step_done
+                  : activeStep === 2
+                  ? classes.step_active
+                  : activeStep < 2 && classes.step_inactive
+              }
+            >
+              <p>2</p>
+            </div>
+            <div className={classes.dashed_line} />
+
+            <div
+              className={
+                activeStep > 3
+                  ? classes.step_done
+                  : activeStep === 3
+                  ? classes.step_active
+                  : activeStep < 3 && classes.step_inactive
+              }
+            >
+              <p>3</p>
+            </div>
           </div>
 
-          <div className="orange_btn">
-            <p>PROCEED</p>
-          </div>
+          {activeStep === 1 ? (
+            <>
+              <div className={classes.input_field_contaier}>
+                <label>First Name</label>
+                <input
+                  placeholder="Enter your First Name"
+                  type="email"
+                  className={classes.input_field}
+                />
+              </div>
+              <div className={classes.input_field_contaier}>
+                <label>Last Name</label>
+                <input
+                  placeholder="Enter your Last Name"
+                  type="email"
+                  className={classes.input_field}
+                />
+              </div>
+
+              <div onClick={handleNextStep} className="orange_btn">
+                <p>PROCEED</p>
+              </div>
+            </>
+          ) : activeStep === 2 ? (
+            <>
+              <div className={classes.input_field_contaier}>
+                <label>Email Address</label>
+                <input
+                  placeholder="Enter your Email Address"
+                  type="email"
+                  className={classes.input_field}
+                />
+              </div>
+
+              <div onClick={handleNextStep} className="orange_btn">
+                <p>PROCEED</p>
+              </div>
+            </>
+          ) : (
+            activeStep === 3 && (
+              <>
+                {!otpSent ? (
+                  <>
+                    <div className={classes.input_field_contaier}>
+                      <label>Phone Number</label>
+                      <input
+                        placeholder="Enter your Phone Number"
+                        type="email"
+                        className={classes.input_field}
+                      />
+                    </div>
+
+                    <div onClick={handleNextStep} className="orange_btn">
+                      <p>PROCEED</p>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className={classes.input_field_contaier}>
+                      <h2>03310 456123</h2>
+                      <label>
+                        Enter the 5-digit ITP code that has been sent from SMS
+                        to complete your account registration.
+                      </label>
+                      <input
+                        placeholder="Enter your Phone Number"
+                        type="email"
+                        className={classes.input_field}
+                      />
+                    </div>
+
+                    <div onClick={handleNextStep} className="orange_btn">
+                      <p>SUBMIT OTP</p>
+                    </div>
+                  </>
+                )}
+              </>
+            )
+          )}
         </div>
       </div>
     </div>
