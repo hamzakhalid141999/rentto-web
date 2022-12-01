12b090c2ebc3420926094c10d967df241ad3027c
Muhammad Nauman <numan98khan@gmail.com>
2022-12-01 05:28:54 +0500

    sr

---
+++ src/ui-components/Group1000004243.jsx
@@ -0,0 +1,391 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, Icon, Text, View } from "@aws-amplify/ui-react";
+export default function Group1000004243(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <View
+      width="1315.44px"
+      height="60px"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      {...rest}
+      {...getOverrideProps(overrides, "Group1000004243")}
+    >
+      <View
+        padding="0px 0px 0px 0px"
+        position="absolute"
+        top="10%"
+        bottom="10%"
+        left="24.33%"
+        right="0%"
+        {...getOverrideProps(overrides, "Group 1000004179")}
+      >
+        <Flex
+          gap="26px"
+          position="absolute"
+          top="25%"
+          bottom="27.08%"
+          left="0%"
+          right="67.45%"
+          width="324px"
+          alignItems="center"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Frame 6")}
+        >
+          <Flex
+            gap="8px"
+            width="fit-content"
+            alignItems="center"
+            shrink="0"
+            height="23px"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Frame 1")}
+          >
+            <Text
+              fontFamily="DM Sans"
+              fontSize="18px"
+              fontWeight="400"
+              color="rgba(0,0,0,1)"
+              lineHeight="21.09375px"
+              textAlign="left"
+              display="flex"
+              direction="column"
+              justifyContent="flex-start"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="List a property"
+              {...getOverrideProps(overrides, "List a property")}
+            ></Text>
+            <View
+              width="16px"
+              height="16px"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              {...getOverrideProps(overrides, "vuesax/linear/arrow-down841196")}
+            ></View>
+          </Flex>
+          <Flex
+            gap="8px"
+            width="fit-content"
+            alignItems="center"
+            shrink="0"
+            height="23px"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Frame 2")}
+          >
+            <Text
+              fontFamily="DM Sans"
+              fontSize="18px"
+              fontWeight="400"
+              color="rgba(0,0,0,1)"
+              lineHeight="21.09375px"
+              textAlign="left"
+              display="flex"
+              direction="column"
+              justifyContent="flex-start"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="Featured"
+              {...getOverrideProps(overrides, "Featured")}
+            ></Text>
+            <View
+              width="16px"
+              height="16px"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              {...getOverrideProps(overrides, "vuesax/linear/arrow-down841199")}
+            ></View>
+          </Flex>
+          <Flex
+            gap="8px"
+            width="fit-content"
+            alignItems="center"
+            shrink="0"
+            height="23px"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Frame 3")}
+          >
+            <Text
+              fontFamily="DM Sans"
+              fontSize="18px"
+              fontWeight="400"
+              color="rgba(0,0,0,1)"
+              lineHeight="21.09375px"
+              textAlign="left"
+              display="flex"
+              direction="column"
+              justifyContent="flex-start"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="FAQ"
+              {...getOverrideProps(overrides, "FAQ")}
+            ></Text>
+            <View
+              width="16px"
+              height="16px"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              {...getOverrideProps(overrides, "vuesax/linear/arrow-down841202")}
+            ></View>
+          </Flex>
+          <Flex
+            gap="8px"
+            width="fit-content"
+            alignItems="center"
+            shrink="0"
+            height="21px"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Frame 4")}
+          ></Flex>
+          <Flex
+            gap="8px"
+            width="fit-content"
+            alignItems="center"
+            shrink="0"
+            height="21px"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Frame 5")}
+          ></Flex>
+        </Flex>
+        <View
+          position="absolute"
+          top="0%"
+          bottom="0%"
+          left="83.86%"
+          right="0%"
+          borderRadius="85px"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(255,144,0,1)"
+          {...getOverrideProps(overrides, "Rectangle 1")}
+        ></View>
+        <Text
+          fontFamily="DM Sans"
+          fontSize="16px"
+          fontWeight="500"
+          color="rgba(255,255,255,1)"
+          lineHeight="18.75px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          position="absolute"
+          top="27.08%"
+          bottom="29.17%"
+          left="87.57%"
+          right="3.51%"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children="Get Started"
+          {...getOverrideProps(overrides, "Get Started")}
+        ></Text>
+        <Text
+          fontFamily="DM Sans"
+          fontSize="18px"
+          fontWeight="400"
+          color="rgba(0,0,0,1)"
+          lineHeight="21.09375px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          position="absolute"
+          top="27.08%"
+          bottom="25%"
+          left="77.96%"
+          right="17.62%"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children="Login"
+          {...getOverrideProps(overrides, "Login")}
+        ></Text>
+      </View>
+      <View
+        padding="0px 0px 0px 0px"
+        width="81.5px"
+        height="20.43px"
+        position="absolute"
+        top="20px"
+        left="944px"
+        {...getOverrideProps(overrides, "Group 1000004480")}
+      >
+        <View
+          padding="0px 0px 0px 0px"
+          position="absolute"
+          top="0%"
+          bottom="0%"
+          left="0%"
+          right="70.76%"
+          {...getOverrideProps(overrides, "Group4766597")}
+        >
+          <View
+            padding="0px 0px 0px 0px"
+            position="absolute"
+            top="0%"
+            bottom="46.67%"
+            left="0.72%"
+            right="0.71%"
+            {...getOverrideProps(overrides, "Group4766598")}
+          >
+            <Icon
+              width="23.4921875px"
+              height="10.895263671875px"
+              viewBox={{
+                minX: 0,
+                minY: 0,
+                width: 23.4921875,
+                height: 10.895263671875,
+              }}
+              paths={[
+                {
+                  d: "M11.7465 10.8953L23.4929 3.06429C22.9822 1.27679 21.3649 0 19.4072 0L4.08572 0C2.12798 0 0.510715 1.27679 0 3.06429L11.7465 10.8953Z",
+                  fill: "rgba(255,144,0,1)",
+                  fillRule: "nonzero",
+                },
+              ]}
+              position="absolute"
+              top="0%"
+              bottom="0%"
+              left="0%"
+              right="0%"
+              {...getOverrideProps(overrides, "Vector4766599")}
+            ></Icon>
+          </View>
+          <View
+            padding="0px 0px 0px 0px"
+            position="absolute"
+            top="24.58%"
+            bottom="0%"
+            left="0%"
+            right="0%"
+            {...getOverrideProps(overrides, "Group4766600")}
+          >
+            <Icon
+              width="23.83203125px"
+              height="15.406494140625px"
+              viewBox={{
+                minX: 0,
+                minY: 0,
+                width: 23.83203125,
+                height: 15.406494140625,
+              }}
+              paths={[
+                {
+                  d: "M12.4274 7.57561C12.2572 7.66073 12.0869 7.74585 11.9167 7.74585C11.7465 7.74585 11.5762 7.66073 11.406 7.57561L0 0L0 11.1506C0 13.534 1.87262 15.4066 4.25596 15.4066L19.5774 15.4066C21.9608 15.4066 23.8334 13.534 23.8334 11.1506L23.8334 0L12.4274 7.57561Z",
+                  fill: "rgba(255,144,0,1)",
+                  fillRule: "nonzero",
+                },
+              ]}
+              position="absolute"
+              top="0%"
+              bottom="0%"
+              left="0%"
+              right="0%"
+              {...getOverrideProps(overrides, "Vector4766601")}
+            ></Icon>
+          </View>
+        </View>
+        <Icon
+          width="23px"
+          height="19.557861328125px"
+          viewBox={{ minX: 0, minY: 0, width: 23, height: 19.557861328125 }}
+          paths={[
+            {
+              d: "M21.6588 2.29362C18.2362 -2.14996 11.5005 0.472027 11.5005 4.98461C11.5005 0.472027 4.76425 -2.15018 1.34116 2.29362C-2.1967 6.88761 1.29033 14.9351 11.5002 19.5579C21.7097 14.9351 25.1967 6.88761 21.6588 2.29362Z",
+              fill: "rgba(255,144,0,1)",
+              fillRule: "nonzero",
+            },
+          ]}
+          position="absolute"
+          top="0%"
+          bottom="4.26%"
+          left="71.78%"
+          right="0%"
+          {...getOverrideProps(overrides, "Vector4766602")}
+        ></Icon>
+      </View>
+      <Icon
+        width="163.36px"
+        height="36.64px"
+        viewBox={{
+          minX: 0,
+          minY: 0,
+          width: 163.35546875,
+          height: 36.63720703125,
+        }}
+        paths={[
+          {
+            d: "M4.21477 26.9955L0 26.9955L0 6.53474L4.21477 6.53474L4.21477 8.22681C5.09282 7.50003 6.13195 6.9916 7.24633 6.74352L7.45449 6.70301C8.13873 6.57518 8.83359 6.51205 9.52978 6.51448L9.52978 10.7525C7.54735 10.7525 6.11477 11.4069 5.23207 12.7156C4.55386 13.718 4.21477 15.0611 4.21477 16.7448L4.21477 26.9955Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M29.2749 18.5817L14.596 18.5817C15.1365 21.3654 16.9217 22.7573 19.9517 22.7573C21.0707 22.7813 22.1799 22.5455 23.1915 22.0686C24.0436 21.6535 24.7349 20.9707 25.1588 20.1257L28.9369 21.9845C28.1821 23.5511 26.946 24.8379 25.4076 25.6585C23.8154 26.5497 21.9952 26.9953 19.947 26.9953C16.9859 26.9953 14.6263 26.0604 12.8682 24.1908C11.1101 22.3211 10.2232 19.8369 10.2075 16.7384C10.2075 13.6223 11.0725 11.1376 12.8024 9.28457C14.5324 7.43149 16.844 6.49664 19.7373 6.48002C22.2696 6.48002 24.3663 7.2035 26.0274 8.65044C27.6885 10.0974 28.7131 12.0668 29.1012 14.5587C29.2125 15.214 29.268 15.8775 29.2671 16.5421L29.2671 18.5676L29.2749 18.5817ZM24.8317 14.5727C24.2348 12.0123 22.5393 10.732 19.7451 10.732C16.951 10.732 15.2555 12.0123 14.6586 14.5727L24.8317 14.5727Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M45.2591 7.38986C46.7678 8.15539 47.9375 9.34525 48.768 10.9594C49.5985 12.5736 50.0232 14.4999 50.042 16.7383L50.042 26.989L45.8162 26.989L45.8162 16.7383C45.8162 15.0546 45.5324 13.7115 44.9648 12.7092C44.2188 11.3869 43.0012 10.7257 41.3119 10.7257C39.6498 10.7257 38.4452 11.3656 37.6982 12.6453C37.118 13.6206 36.8207 14.9362 36.8061 16.5919L36.8061 26.989L32.5944 26.989L32.5944 6.51421L36.8092 6.51421L36.8092 7.74665C36.9892 7.62045 37.1754 7.50204 37.3695 7.39142C38.5998 6.80043 39.9482 6.49349 41.3143 6.49349C42.6803 6.49349 44.0288 6.80043 45.2591 7.39142L45.2591 7.38986Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M54.7414 11.0857L51.7943 10.7321L51.7943 6.49408L54.7414 6.84777L54.7414 0L58.9577 0L58.9577 6.84777L61.9063 6.49408L61.9063 10.7321L58.9577 11.0857L58.9577 20.59C58.9577 21.4532 59.1445 22.0588 59.518 22.4068C59.7945 22.6436 60.2171 22.762 60.7857 22.762L61.8813 22.762L61.8813 27L60.7857 27C58.9713 27 57.5105 26.4084 56.4035 25.2253C55.2965 24.0422 54.7424 22.4971 54.7414 20.59L54.7414 11.0857Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M66.6884 11.0857L63.7398 10.7321L63.7398 6.49408L66.6884 6.84777L66.6884 0L70.9032 0L70.9032 6.84777L73.8518 6.49408L73.8518 10.7321L70.9032 11.0857L70.9032 20.59C70.9032 21.4532 71.09 22.0588 71.4635 22.4068C71.74 22.6436 72.162 22.762 72.7296 22.762L73.8252 22.762L73.8252 27L72.7296 27C70.9173 27 69.4565 26.4084 68.3474 25.2253C67.2383 24.0422 66.6848 22.4971 66.6869 20.59L66.6869 11.0857L66.6884 11.0857Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M85.2059 6.49407C88.0982 6.49407 90.4093 7.42892 92.1392 9.29861C93.8691 11.1683 94.7346 13.6483 94.7357 16.7385C94.7357 19.8546 93.8702 22.3387 92.1392 24.1908C90.4082 26.0428 88.0971 26.9777 85.2059 26.9953C82.3115 26.9953 79.9999 26.0605 78.271 24.1908C76.5421 22.3211 75.6772 19.837 75.6761 16.7385C75.6761 13.6223 76.5411 11.1377 78.271 9.28459C80.001 7.43151 82.3126 6.50134 85.2059 6.49407ZM85.2059 22.7573C87.1988 22.7573 88.6381 22.1029 89.524 20.7941C90.2022 19.7783 90.5413 18.4279 90.5413 16.7431C90.5413 15.0583 90.2022 13.708 89.524 12.6921C88.6381 11.3844 87.1988 10.7305 85.2059 10.7305C83.213 10.7305 81.7737 11.3828 80.8878 12.6874C80.2232 13.7033 79.8909 15.0537 79.8909 16.7385C79.8909 18.4233 80.2232 19.7736 80.8878 20.7895C81.7726 22.1014 83.212 22.7573 85.2059 22.7573Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M112.692 16.7119C112.692 18.4056 112.349 19.7563 111.663 20.7642C110.767 22.0799 109.319 22.7378 107.317 22.7378C105.301 22.7378 103.846 22.0729 102.95 20.7432C102.278 19.7354 101.942 18.3846 101.942 16.6909C101.942 14.9972 102.278 13.6395 102.95 12.6177C103.846 11.3019 105.301 10.644 107.317 10.644C109.319 10.644 110.767 11.3019 111.663 12.6177C112.349 13.6255 112.692 14.9762 112.692 16.6699L112.692 16.7119ZM112.692 8.08252C111.824 7.35465 110.788 6.86475 109.584 6.61279L109.375 6.5498C108.717 6.43783 108.031 6.38184 107.317 6.38184C106.603 6.38184 105.917 6.43783 105.259 6.5498L105.049 6.61279C102.768 7.06071 100.969 8.1735 99.6533 9.95117C98.3376 11.7428 97.6797 13.9894 97.6797 16.6909C97.6797 19.3784 98.3376 21.611 99.6533 23.3887C100.941 25.1663 102.719 26.2931 104.986 26.769L105.217 26.811C105.889 26.937 106.589 27 107.317 27C108.045 27 108.745 26.937 109.417 26.811L109.647 26.769C110.823 26.5031 111.838 26.0132 112.692 25.2993L112.692 27L116.954 27L116.954 6.40283L112.692 6.40283L112.692 8.08252Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M125.709 16.6489C125.709 14.9552 126.052 13.6045 126.738 12.5967C127.634 11.2809 129.083 10.623 131.084 10.623C133.1 10.623 134.556 11.2879 135.452 12.6177C136.124 13.6255 136.459 14.9832 136.459 16.6909C136.459 18.3846 136.124 19.7354 135.452 20.7432C134.556 22.0729 133.1 22.7378 131.084 22.7378C129.083 22.7378 127.634 22.0799 126.738 20.7642C126.052 19.7703 125.709 18.4266 125.709 16.7329L125.709 16.6489ZM129.027 26.811C129.685 26.937 130.371 27 131.084 27C131.798 27 132.484 26.937 133.142 26.811L133.352 26.769C135.634 26.3071 137.432 25.1873 138.748 23.4097C140.064 21.618 140.722 19.3784 140.722 16.6909C140.722 14.0034 140.071 11.7638 138.769 9.97217C137.467 8.2085 135.683 7.0887 133.415 6.61279L133.184 6.5708C132.512 6.44482 131.812 6.38184 131.084 6.38184C130.357 6.38184 129.657 6.44482 128.985 6.5708L128.775 6.61279C127.585 6.87874 126.563 7.36865 125.709 8.08252L125.709 6.38184L121.447 6.38184L121.447 36.6372L125.709 36.6372L125.709 25.2783C126.577 26.0062 127.62 26.5031 128.838 26.769L129.027 26.811Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+          {
+            d: "M148.343 16.6489C148.343 14.9552 148.686 13.6045 149.372 12.5967C150.268 11.2809 151.717 10.623 153.718 10.623C155.734 10.623 157.19 11.2879 158.085 12.6177C158.757 13.6255 159.093 14.9832 159.093 16.6909C159.093 18.3846 158.757 19.7354 158.085 20.7432C157.19 22.0729 155.734 22.7378 153.718 22.7378C151.717 22.7378 150.268 22.0799 149.372 20.7642C148.686 19.7703 148.343 18.4266 148.343 16.7329L148.343 16.6489ZM151.661 26.811C152.319 26.937 153.004 27 153.718 27C154.432 27 155.118 26.937 155.776 26.811L155.986 26.769C158.267 26.3071 160.066 25.1873 161.382 23.4097C162.698 21.618 163.355 19.3784 163.355 16.6909C163.355 14.0034 162.705 11.7638 161.403 9.97217C160.101 8.2085 158.316 7.0887 156.049 6.61279L155.818 6.5708C155.146 6.44482 154.446 6.38184 153.718 6.38184C152.99 6.38184 152.291 6.44482 151.619 6.5708L151.409 6.61279C150.219 6.87874 149.197 7.36865 148.343 8.08252L148.343 6.38184L144.081 6.38184L144.081 36.6372L148.343 36.6372L148.343 25.2783C149.211 26.0062 150.254 26.5031 151.472 26.769L151.661 26.811Z",
+            fill: "rgba(255,144,0,1)",
+            fillRule: "nonzero",
+          },
+        ]}
+        position="absolute"
+        top="17px"
+        left="12px"
+        {...getOverrideProps(overrides, "Vector49810968")}
+      ></Icon>
+    </View>
+  );
+}
